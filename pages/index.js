51f8aa2022110c9eeb5ef576f88215b7ae0a4184
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-11-07 22:24:33 -0800

    Made dummy-data using coffee-store.json file.

--- pages/index.js
+++ pages/index.js
@@ -26,11 +26,14 @@ export default function Home() {
         <div className={styles.heroImage}>
           <Image src="/static/hero.png" width={700} height={400} />
         </div>
-        <Card
-          name="DarkHorse Coffee"
-          imgUrl="/hero-image.png"
-          href="/coffee-store/darkhorse-coffee"
-        />
+        <div className={styles.cardLayout}>
+          <Card
+            className={styles.card}
+            name="DarkHorse Coffee"
+            imgUrl="/hero-image.png"
+            href="/coffee-store/darkhorse-coffee"
+          />
+        </div>
       </main>
     </div>
   );
