7488569f8dbb3152b2546006b2b76a26bd3417d3
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-11-07 22:09:18 -0800

    Made card component, making basic structure & started styling. Also installed classnames package.

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 
 import Banner from "../components/Banner";
+import Card from "../components/card";
 
 import styles from "../styles/Home.module.css";
 
@@ -25,6 +26,11 @@ export default function Home() {
         <div className={styles.heroImage}>
           <Image src="/static/hero.png" width={700} height={400} />
         </div>
+        <Card
+          name="DarkHorse Coffee"
+          imgUrl="/hero-image.png"
+          href="/coffee-store/darkhorse-coffee"
+        />
       </main>
     </div>
   );
