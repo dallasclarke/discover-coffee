c480cb135edca224c330d2af094aa3db1fac1728
Dallas Clarke <65244012+dallasclarke@users.noreply.github.com>
2021-11-09 00:01:28 -0800

    Conditionally rendering data setup for coffee stores. I also changed coffeeStore name to coffeeStoreData to be more clear.

--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ import Image from "next/image";
 import Banner from "../components/Banner";
 import Card from "../components/card";
 
-import coffeeStores from "../data/coffee-store.json";
+import coffeeStoresData from "../data/coffee-store.json";
 
 import styles from "../styles/Home.module.css";
 
 export async function getStaticProps(context) {
   return {
     props: {
-      coffeeStores,
+      coffeeStores: coffeeStoresData,
     },
   };
 }
@@ -36,19 +36,24 @@ export default function Home(props) {
         <div className={styles.heroImage}>
           <Image src="/static/hero.png" width={700} height={400} />
         </div>
-        <div className={styles.cardLayout}>
-          {props.coffeeStores.map((coffeeStore) => {
-            return (
-              <Card
-                key={coffeeStore.id}
-                className={styles.card}
-                name={coffeeStore.name}
-                imgUrl={coffeeStore.imgUrl}
-                href={`/coffee-store/${coffeeStore.id}`}
-              />
-            );
-          })}
-        </div>
+        {props.coffeeStores.length > 0 && (
+          <>
+            <h2 className={styles.heading2}>Toronto Stores</h2>
+            <div className={styles.cardLayout}>
+              {props.coffeeStores.map((coffeeStore) => {
+                return (
+                  <Card
+                    key={coffeeStore.id}
+                    className={styles.card}
+                    name={coffeeStore.name}
+                    imgUrl={coffeeStore.imgUrl}
+                    href={`/coffee-store/${coffeeStore.id}`}
+                  />
+                );
+              })}
+            </div>
+          </>
+        )}
       </main>
     </div>
   );
